6a223b9cfef45ed103c1c618aa4baee28811575a
Artemisia <lihongyueli@umass.edu>
2026-09-18 12:41:03 -0400

    add About page

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 
 const app = express();
+app.set("view engine", "ejs");
 const PORT = 3000;
 
 const projects = [
   { name: 'Weather app', tag: 'javascript' },
   { name: 'Portfolio site', tag: 'express' },
   { name: 'Budget tracker', tag: 'python' },
+];
+
+const events = [
+  { title: 'Career fair' },
+  { title: 'Hackathon kickoff' },
 ];
 
 app.get('/', (req, res) => {
@@ -21,6 +27,14 @@ app.get('/projects', (req, res) => {
   }
   res.json(projects.filter(project => project.tag === tag))
 
+});
+
+app.get('/events', (req, res) => {
+  res.render('events', { events });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
 app.listen(PORT, () => {
